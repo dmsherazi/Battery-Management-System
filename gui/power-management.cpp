882753dd004fad0be176539f200dc51268a983e6
Ken Sarkies <ksarkies@internode.on.net>
2014-10-31 16:56:37 +1030

    Command line options

--- gui/power-management.cpp
+++ gui/power-management.cpp
@@ -33,6 +33,7 @@ Uses: Qt version 4.8.1
  *   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.              *
  ***************************************************************************/
 
+#include <unistd.h>
 #include "power-management-main.h"
 #include <QApplication>
 #include <QMessageBox>
@@ -44,8 +45,48 @@ Uses: Qt version 4.8.1
 
 int main(int argc,char ** argv)
 {
+/* Interpret any command line options */
+    QString inPort = SERIAL_PORT;
+    int c;
+    uint initialBaudrate = 4;
+    int baudParm;
+    opterr = 0;
+    while ((c = getopt (argc, argv, "P:b:")) != -1)
+    {
+        switch (c)
+        {
+        case 'P':
+            inPort = optarg;
+            break;
+        case 'b':
+            baudParm = atoi(optarg);
+            switch (baudParm)
+            {
+            case 2400: initialBaudrate=0;break;
+            case 4800: initialBaudrate=1;break;
+            case 9600: initialBaudrate=2;break;
+            case 19200: initialBaudrate=3;break;
+            case 38400: initialBaudrate=4;break;
+            case 57600: initialBaudrate=5;break;
+            case 115200: initialBaudrate=6;break;
+            default:
+                fprintf (stderr, "Invalid Baudrate %i.\n", baudParm);
+                return false;
+            }
+            break;
+        case '?':
+            if (optopt == 'P')
+                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+            else if (isprint (optopt))
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            else
+                fprintf (stderr,"Unknown option character `\\x%x'.\n",optopt);
+            default: return false;
+        }
+    }
+
     QApplication application(argc,argv);
-    PowerManagementGui powerManagementGui;
+    PowerManagementGui powerManagementGui(inPort,initialBaudrate);
     if (powerManagementGui.success())
     {
         powerManagementGui.show();
